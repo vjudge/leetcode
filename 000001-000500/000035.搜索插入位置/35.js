00588003782537a65fd031ea1dd5b01c1b21f014
vjudge <gradonday@gmail.com>
2021-07-12 22:14:23 +0800

    add 35

---
+++ 000001-000500/000035.搜索插入位置/35.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0
+    let right = nums.length
+    let mid = 0
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2)
+        if ( nums[mid] == target ) {
+            break
+        } else if (nums[mid] < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return (nums[mid] < target) ? (mid + 1) : mid
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5))
+console.log(searchInsert([1, 3, 5, 6], 2))
+console.log(searchInsert([1, 3, 5, 6], 7))
+console.log(searchInsert([1, 3, 5, 6], 0))
+console.log(searchInsert([1, 3], 2))
