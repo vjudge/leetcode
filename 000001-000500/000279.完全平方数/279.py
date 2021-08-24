5b7997a4e6fc5ea620ec789eb58b9d8631f8f490
vjudge <gradonday@gmail.com>
2021-08-24 23:10:13 +0800

    add 279.py

---
+++ 000001-000500/000279.完全平方数/279.py
@@ -0,0 +1,13 @@
+import math
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [x for x in range(0, n+1)]
+        for i in range(1, n+1):
+            for j in range(0, int(math.sqrt(i))+1):
+                dp[i] = min(dp[i], dp[i - j ** 2] + 1)
+        return dp[n]
+        
+
+s = Solution()
+print(s.numSquares(12))
