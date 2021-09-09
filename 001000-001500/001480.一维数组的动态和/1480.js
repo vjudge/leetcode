f4b5dddc9041aa927651c8d5c64cc35a0ad68cf8
vjudge <gradonday@gmail.com>
2021-09-09 22:07:10 +0800

    add 1480

---
+++ 001000-001500/001480.一维数组的动态和/1480.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] = nums[i - 1] + nums[i]
+  }
+  return nums
+};
+
+console.log(runningSum([1, 2, 3, 4]))
+console.log(runningSum([1, 1, 1, 1, 1]))
+console.log(runningSum([3, 1, 2, 10, 1]))
