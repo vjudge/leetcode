b3fa1272a0919ae4edced8a8503dcf6cb7e53db8
vjudge <gradonday@gmail.com>
2021-09-09 22:59:46 +0800

    update 64

--- 剑指Offer/0064.求1+2+…+n/64.js
+++ 剑指Offer/0064.求1+2+…+n/64.js
@@ -2,7 +2,19 @@
  * @param {number} n
  * @return {number}
  */
+// 方法一：用时92ms
+// 执行用时超过了 11%
+// 消耗内存超过了 97%
 var sumNums = function(n) {
     return (1 + n) * n / 2
 };
+
+// 方法二：用时 64ms
+// 执行用时超过了 92%
+// 消耗内存超过了 85%
+var sumNums = function(n) {
+    return (1 + n) * n >> 1
+};
+
+
 console.log(sumNums(10))
