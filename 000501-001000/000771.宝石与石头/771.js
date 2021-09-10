95cb73b0402595094d4fd7371a4b026e07033677
vjudge <gradonday@gmail.com>
2021-09-10 23:42:04 +0800

    update 771

--- 000501-001000/000771.宝石与石头/771.js
+++ 000501-001000/000771.宝石与石头/771.js
@@ -20,5 +20,17 @@ var numJewelsInStones = function(jewels, stones) {
   return cnt
 };
 
+
+// 方法二：用时 68ms
+// 执行用时超过了 92%
+// 消耗内存超过了 53%
+var numJewelsInStones = function(jewels, stones) {
+  const jewSet = new Set()
+  jewels.split('').forEach(c => jewSet.add(c))
+  return stones.split('').filter(c => jewSet.has(c)).length
+};
+
+
+
 console.log(numJewelsInStones('aA', 'aAAbbbb'))
 console.log(numJewelsInStones('z', 'ZZ'))
