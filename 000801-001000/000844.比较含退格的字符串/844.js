2f9ca13f98209aee0f15bd7c1c4575d49eae0e15
vjudge <gradonday@gmail.com>
2021-09-14 09:33:47 +0800

    update 0008001-001000

---
+++ 000801-001000/000844.比较含退格的字符串/844.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} S
+ * @param {string} T
+ * @return {boolean}
+ */
+// 战胜89%
+var backspaceCompare = function(S, T) {
+    let sRst = handleStr(S)
+    let tRst = handleStr(T)
+    console.log(` S = `, sRst)
+    console.log(` T = `, tRst)
+    if (sRst == tRst) {
+        return true
+    }
+    return false
+};
+
+var handleStr = function (str) {
+    let result = ''
+    let cnt = 0
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (str[i] === '#') {
+            cnt ++
+            continue
+        } else if (cnt > 0) {
+            cnt --
+            continue
+        }
+        result = str[i] + result
+    }
+    return result
+}
+
+console.log('result = ', backspaceCompare("bxj##tw", "bxj###tw"))
+// console.log('result = ', backspaceCompare("ab#c", "ad#c"))
+// console.log('result = ', backspaceCompare("ab##", "c#d#"))
+// console.log('result = ', backspaceCompare("a##c", "#a#c"))
+// console.log('result = ', backspaceCompare("a#c", "b"))
---
+++ 000801-001000/README.md
@@ -0,0 +1,14 @@
+# leetcode
+
+
+### []()
+### []()
+### [000844.比较含退格的字符串](https://github.com/vjudge/leetcode/tree/master/000801-001000/000844.比较含退格的字符串)
+### []()
+### []()
+### [000905.按奇偶排序数组](https://github.com/vjudge/leetcode/tree/master/000801-001000/000905.按奇偶排序数组)
+### []()
+### [000961.重复N次的元素](https://github.com/vjudge/leetcode/tree/master/000801-001000/000961.重复N次的元素)
+### []()
+### [000977.有序数组的平方](https://github.com/vjudge/leetcode/tree/master/000801-001000/000977.有序数组的平方)
+### []()
