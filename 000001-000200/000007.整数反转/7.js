98e55f1728d2a6471475bb19f2435f8b14571d62
vjudge <gradonday@gmail.com>
2021-09-14 11:39:00 +0800

    update 7

--- 000001-000200/000007.整数反转/7.js
+++ 000001-000200/000007.整数反转/7.js
@@ -15,16 +15,28 @@
 // };
 
 
-// 方法二：用时 88ms
-// 执行用时超过了 64%
-// 消耗内存超过了 69%
+// 方法二：用时 96ms
+// 执行用时超过了 42%
+// 消耗内存超过了 40%
 var reverse = function(x) {
-  let result = 0
-  while (x) {
-    result = result * 10 + x % 10
-    x = x / 10 | 0
+  let numstr = Array.from(String(x))
+  let start = 0
+  let end = numstr.length - 1
+  if (numstr[0] === '-') {
+    start = 1
   }
-  return (result | 0) === result ? result : 0
+  while (start < end) {
+    let tmp = numstr[start]
+    numstr[start] = numstr[end]
+    numstr[end] = tmp
+    start ++
+    end --
+  }
+  let result = parseInt(numstr.join(''))
+  if (result >= 2 ** 31 || result <= -((2 ** 31) - 1)) {
+    return 0
+  }
+  return result
 };
 
 
