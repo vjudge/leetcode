fa4cb50ed1a61b1210adae3e89c97b4abaf1eb7c
vjudge <gradonday@gmail.com>
2021-09-15 15:53:25 +0800

    add tree sample

---
+++ tools/tree-sample/tree.js
@@ -0,0 +1,26 @@
+function genTree (trvals) {
+  let root = new TreeNode(trvals[0])
+  let queue = [root]
+  let start = 1
+  while (queue.length && start < trvals.length) {
+    let curnode = queue.shift()
+    if (trvals[start]) {
+      curnode.left = new TreeNode(trvals[start])
+      queue.push(curnode.left)
+    }
+    start ++
+    if (trvals[start]) {
+      curnode.right = new TreeNode(trvals[start])
+      queue.push(curnode.right)
+    }
+    start ++
+  }
+  return root
+}
+
+
+function TreeNode(val, left, right) {
+  this.val = (val===undefined ? 0 : val)
+  this.left = (left===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
+}
