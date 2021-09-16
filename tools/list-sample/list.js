2ae42755679d81ae9c83482258c5555c6a92581c
vjudge <gradonday@gmail.com>
2021-09-16 20:55:11 +0800

    update list

--- tools/list-sample/list.js
+++ tools/list-sample/list.js
@@ -1,23 +1,25 @@
-function ListNode (val, next) {
-  this.val = (val===undefined ? 0 : val)
-  this.next = (next===undefined ? null : next)
+function main (trvals, n) {
+  let head = genList(trvals)
+  return hasPathSum(head, n)
 }
 
-function genList (l1) {
-  let lnode1 = null
+function genList (trvals) {
+  let result = null
   let head = null
-  for (let i = 0; i < l1.length; i++) {
-    curnode = new ListNode(l1[i])
-    if (!lnode1) {
-      lnode1 = curnode
-      head = lnode1
+  for (let i = 0; i < trvals.length; i++) {
+    curnode = new ListNode(trvals[i])
+    if (!result) {
+      result = curnode
+      head = result
     } else {
       head.next = curnode
       head = head.next
     }
   }
-  console.log('l1: ', lnode1)
-  // return isPalindrome(lnode1)
+  return result
 }
 
-console.log(genList([1, 2, 2, 1]))
+function ListNode (val, next) {
+  this.val = (val===undefined ? 0 : val)
+  this.next = (next===undefined ? null : next)
+}
