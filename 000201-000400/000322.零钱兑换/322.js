6253323a0cc2f8e6541e5f5cff7d5b7ed4ccab30
vjudge <gradonday@gmail.com>
2021-09-16 15:59:47 +0800

    add 322

---
+++ 000201-000400/000322.零钱兑换/322.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+// 方法一：用时 96ms
+// 执行用时超过了 96%
+// 消耗内存超过了 35%
+var coinChange = function(coins, amount) {
+  if(!amount) {
+    return 0
+  }
+
+  let dp = Array(amount + 1).fill(Infinity)
+  dp[0] = 0
+  for(let i =0; i < coins.length; i++) {
+    for(let j = coins[i]; j <= amount; j++) {
+      dp[j] = Math.min(dp[j - coins[i]] + 1, dp[j])
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount]
+};
+
+console.log(coinChange([1, 2, 5], 11))
+console.log(coinChange([2], 3))
+console.log(coinChange([1], 0))
+console.log(coinChange([1], 1))
+console.log(coinChange([1], 2))
