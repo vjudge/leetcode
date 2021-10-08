013ea169fbe691456a57206a8e4d2a0a08cdcb61
vjudge <gradonday@gmail.com>
2021-10-08 23:54:26 +0800

    add 62

---
+++ 剑指Offer/0062.圆圈中最后剩下的数字/62.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// 方法一：用时 4ms
+// 执行用时超过了 100%
+// 消耗内存超过了 33%
+func lastRemaining(n int, m int) int {
+	ret := 0
+	for i := 2; i != n + 1; i++ {
+		ret = (m + ret) % i
+	}
+	return ret
+}
+
+func main ()  {
+	fmt.Println("ret:", lastRemaining(5, 3))
+	fmt.Println("ret:", lastRemaining(10, 17))
+}
