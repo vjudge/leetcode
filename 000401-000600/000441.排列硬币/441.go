3efe5745e5a2c72049b1d12ad194a3ee22633185
vjudge <gradonday@gmail.com>
2021-10-10 11:47:53 +0800

    add 441go

--- 000401-000600/000441.排列硬币/441.go
+++ 000401-000600/000441.排列硬币/441.go
@@ -1,1 +1,27 @@
-package _00441_排列硬币
+package main
+
+import (
+	"fmt"
+)
+
+// 方法二: 用时 4ms
+// 执行用时超过了 73%
+// 消耗内存超过了 99%
+func arrangeCoins(n int) int {
+	left := 0
+	right := n
+	for ;left < right; {
+		mid := int((right - left + 1) / 2) + left
+		if (mid + 1) * mid <= 2 * n {
+			left = mid
+		} else {
+			right = mid - 1
+		}
+	}
+	return left
+}
+
+func main ()  {
+	fmt.Println(arrangeCoins(5))
+	fmt.Println(arrangeCoins(8))
+}
