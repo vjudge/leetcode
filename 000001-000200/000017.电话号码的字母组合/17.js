1425f69cefaced6094a24cce0e2434c61ce0c672
vjudge <gradonday@gmail.com>
2021-10-10 11:24:24 +0800

    add 17js

--- 000001-000200/000017.电话号码的字母组合/17.js
+++ 000001-000200/000017.电话号码的字母组合/17.js
@@ -2,23 +2,33 @@
  * @param {string} digits
  * @return {string[]}
  */
-// 方法一：用时 164ms
-// 执行用时超过了 87%
-// 消耗内存超过了 98%
+// 方法一：用时 64ms
+// 执行用时超过了 85%
+// 消耗内存超过了 83%
+// 难度: 中等
 var letterCombinations = function (digits) {
-  let letters =['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
-
-
-
-  var dfs = function (s, k, prestr) {
-    if (k == 1) {
-      return map[s].split('')
-    }
+  if (!digits.length) {
+    return []
   }
+  let letters = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+  let ret = []
+  for (let i = 0; i < digits.length; i++) {
+    let index = digits[i]
+    if (!ret.length) {
+      ret = letters[index].split('')
+      continue
+    }
+    let tmp = []
+    for (let k = 0; k < ret.length; k++) {
+      for (let j = 0; j < letters[index].length; j++) {
+        tmp.push(ret[k] + letters[index][j])
+      }
+    }
+    ret = tmp
+  }
+  return ret
 };
 
-
-
 console.log(letterCombinations('23'))
-console.log(letterCombinations('2'))
-console.log(letterCombinations(''))
+// console.log(letterCombinations('2'))
+// console.log(letterCombinations(''))
