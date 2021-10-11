826ea50f1990a232e0131d53bafa8b0a455662b4
vjudge <gradonday@gmail.com>
2021-10-11 11:55:01 +0800

    add 860go

--- 000801-001000/000860.柠檬水找零/860.go
+++ 000801-001000/000860.柠檬水找零/860.go
@@ -1,1 +1,37 @@
-package _00860_柠檬水找零
+package main
+
+import "fmt"
+
+// 方法一：用时 116ms
+// 执行用时超过了 18%
+// 消耗内存超过了 68%
+func lemonadeChange(bills []int) bool {
+	five := 0
+	ten := 0
+	for i := 0; i < len(bills); i++ {
+		if bills[i] == 5 {
+			five ++
+		} else if bills[i] == 10 {
+			if five == 0 {
+				return false
+			}
+			five --
+			ten ++
+		} else {
+			if five > 0 && ten > 0 {
+				five --
+				ten --
+			} else if five > 3 {
+				 five -= 3
+			} else {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func main ()  {
+	fmt.Println(lemonadeChange([]int{5, 5, 5, 10, 20}))
+	fmt.Println(lemonadeChange([]int{5, 5, 10, 10, 20}))
+}
