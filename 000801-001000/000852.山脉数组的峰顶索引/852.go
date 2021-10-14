31332af3936b94544714f82c89c09e427ff141cf
vjudge <gradonday@gmail.com>
2021-10-14 16:27:24 +0800

    add 852go

---
+++ 000801-001000/000852.山脉数组的峰顶索引/852.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+//方法一: 用时 8 ms
+// 执行用时超过了 83 %
+// 消耗内存超过了 100 %
+func peakIndexInMountainArray(arr []int) int {
+	ret := 1
+	for ;ret < len(arr); ret ++ {
+		if arr[ret] > arr[ret - 1] && arr[ret] > arr[ret + 1] {
+			break
+		}
+	}
+	return ret
+}
+
+func main ()  {
+	fmt.Println(peakIndexInMountainArray([]int{0, 1, 0}))
+	fmt.Println(peakIndexInMountainArray([]int{0, 2, 1, 0}))
+	fmt.Println(peakIndexInMountainArray([]int{0, 10, 5, 2}))
+	fmt.Println(peakIndexInMountainArray([]int{3, 4, 5, 1}))
+	fmt.Println(peakIndexInMountainArray([]int{24, 69, 100, 99, 79, 78, 67, 36, 26, 19}))
+}
