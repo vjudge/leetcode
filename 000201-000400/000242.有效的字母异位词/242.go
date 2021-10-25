435ec073aad3f91923d2c5705e252d3760f88c67
vjudge <gradonday@gmail.com>
2021-10-25 18:45:57 +0800

    add 242go

--- 000201-000400/000242.有效的字母异位词/242.go
+++ 000201-000400/000242.有效的字母异位词/242.go
@@ -1,1 +1,27 @@
-package _00242_有效的字母异位词
+package main
+
+import "fmt"
+
+// 方法一：用时 0 ms
+// 执行用时超过了 100 %
+// 消耗内存超过了 100 %
+// 难度: 简单
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	stotal := [26]int{}
+	ttotal := [26]int{}
+	for _, item := range s {
+		stotal[item - 'a'] ++
+	}
+	for _, item := range t {
+		ttotal[item - 'a'] ++
+	}
+	return stotal == ttotal
+}
+
+func main ()  {
+	fmt.Println(isAnagram("anagram", "nagaram"))
+	fmt.Println(isAnagram("rat", "cat"))
+}
