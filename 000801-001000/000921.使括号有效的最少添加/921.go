31f21d8ec453773f21e8918461a444349bc3effc
vjudge <gradonday@gmail.com>
2021-10-25 21:57:36 +0800

    add 921go

--- 000801-001000/000921.使括号有效的最少添加/921.go
+++ 000801-001000/000921.使括号有效的最少添加/921.go
@@ -1,1 +1,31 @@
-package _00921_使括号有效的最少添加
+package main
+
+import "fmt"
+
+// 方法一：用时 0 ms
+// 执行用时超过了 100 %
+// 消耗内存超过了 100 %
+// 难度: 中等
+func minAddToMakeValid(s string) int {
+	ret := 0
+	mstack := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == ')' {
+			if mstack > 0 {
+				mstack --
+				continue
+			}
+			ret ++
+			continue
+		}
+		mstack++
+	}
+	return ret + mstack
+}
+
+func main ()  {
+	fmt.Println(minAddToMakeValid("())"))
+	fmt.Println(minAddToMakeValid("((("))
+	fmt.Println(minAddToMakeValid("()"))
+	fmt.Println(minAddToMakeValid("()))(("))
+}
