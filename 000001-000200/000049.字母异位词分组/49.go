cb1a99034a20f29fb299b0dc52da333c3d6e7e4b
vjudge <gradonday@gmail.com>
2021-10-26 11:24:32 +0800

    add 49go

--- 000001-000200/000049.字母异位词分组/49.go
+++ 000001-000200/000049.字母异位词分组/49.go
@@ -1,1 +1,31 @@
-package _00049_字母异位词分组
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// 方法一：用时 20 ms
+// 执行用时超过了 89 %
+// 消耗内存超过了 69 %
+// 难度: 中等
+func groupAnagrams(strs []string) [][]string {
+	smap := map[string][]string{}
+	for _, str := range strs {
+		s := []byte(str)
+		sort.Slice(s, func(i, j int) bool { return s[i] < s[j] })
+		key := string(s)
+		smap[key] = append(smap[key], str)
+	}
+	ret := make([][]string, 0, len(smap))
+	for _, v := range smap {
+		ret = append(ret, v)
+	}
+	return ret
+}
+
+func main ()  {
+	fmt.Println(groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"}))
+	fmt.Println(groupAnagrams([]string{""}))
+	fmt.Println(groupAnagrams([]string{"a"}))
+}
