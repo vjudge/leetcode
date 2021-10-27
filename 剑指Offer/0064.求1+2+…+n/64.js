f2666d892d10d5f2b492ecea2ef0ac1a736189eb
vjudge <gradonday@gmail.com>
2021-10-27 22:41:49 +0800

    update 64

--- 剑指Offer/0064.求1+2+…+n/64.js
+++ 剑指Offer/0064.求1+2+…+n/64.js
@@ -2,9 +2,9 @@
  * @param {number} n
  * @return {number}
  */
-// 方法一：用时 92ms
-// 执行用时超过了 11%
-// 消耗内存超过了 97%
+// 方法一：用时 92 ms
+// 执行用时超过了 11 %
+// 消耗内存超过了 97 %
 // 难度: 中等
 var sumNums = function(n) {
     return (1 + n) * n / 2
