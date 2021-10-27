3dfb61ece2016917e98e2c799dcc278c65feac13
vjudge <gradonday@gmail.com>
2021-10-27 10:28:56 +0800

    update 1991go

--- 001801-002000/001991.找到数组的中间位置/1991.go
+++ 001801-002000/001991.找到数组的中间位置/1991.go
@@ -6,6 +6,8 @@ import "fmt"
 // 执行用时超过了 100 %
 // 消耗内存超过了 79 %
 // 难度: 简单
+// 同: https://leetcode-cn.com/problems/find-pivot-index
+// 同: https://github.com/vjudge/leetcode/tree/master/000601-000800/000724.寻找数组的中心下标
 func findMiddleIndex (nums []int) int {
 	sum := 0
 	for _, num := range nums {
