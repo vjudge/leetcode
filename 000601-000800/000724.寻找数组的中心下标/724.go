89e87e3eff7df92b1aa78180958c51cd58e5b2aa
vjudge <gradonday@gmail.com>
2021-10-27 10:19:59 +0800

    add 724go

--- 000601-000800/000724.寻找数组的中心下标/724.go
+++ 000601-000800/000724.寻找数组的中心下标/724.go
@@ -1,1 +1,32 @@
-package _00724_寻找数组的中心下标
+package main
+
+import "fmt"
+
+// 方法一：用时 0 ms
+// 执行用时超过了 100 %
+// 消耗内存超过了 79 %
+// 难度: 简单
+func pivotIndex(nums []int) int {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	midsum := 0
+	for i, num := range nums {
+		if 2 * midsum + num == sum {
+			return i
+		}
+		midsum += num
+	}
+	return -1
+}
+
+func main ()  {
+	fmt.Println(pivotIndex([]int{1, 7, 3, 6, 5, 6}))
+	fmt.Println(pivotIndex([]int{1, 2, 3}))
+	fmt.Println(pivotIndex([]int{2, 1, -1}))
+	fmt.Println(pivotIndex([]int{2, 3, -1, 8, 4}))
+	fmt.Println(pivotIndex([]int{1, -1, 4}))
+	fmt.Println(pivotIndex([]int{2, 5}))
+	fmt.Println(pivotIndex([]int{1}))
+}
