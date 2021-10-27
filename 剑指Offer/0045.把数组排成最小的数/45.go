8b7d78dfad16610359843e36f81fa9f75566717d
vjudge <gradonday@gmail.com>
2021-10-27 22:38:17 +0800

    add 45go

--- 剑指Offer/0045.把数组排成最小的数/45.go
+++ 剑指Offer/0045.把数组排成最小的数/45.go
@@ -1,1 +1,18 @@
-package _045_把数组排成最小的数
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// 方法一：用时 76 ms
+// 执行用时超过了 54 %
+// 消耗内存超过了 15 %
+// 难度: 中等
+func minNumber(nums []int) string {
+}
+
+func main ()  {
+	fmt.Println(minNumber([]int{3, 30, 34, 5, 9}))
+	fmt.Println(minNumber([]int{10, 2}))
+}
