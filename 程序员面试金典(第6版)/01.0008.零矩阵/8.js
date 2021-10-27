fab6d1a58d7ef1fc233838eda74c8cb5e2fc4fa2
vjudge <gradonday@gmail.com>
2021-10-27 12:23:11 +0800

    add 8js

--- 程序员面试金典(第6版)/01.0008.零矩阵/8.js
+++ 程序员面试金典(第6版)/01.0008.零矩阵/8.js
@@ -3,11 +3,11 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 // 方法一：用时 84 ms
-// 执行用时超过了 86 %
-// 消耗内存超过了 82 %
+// 执行用时超过了 89 %
+// 消耗内存超过了 85 %
 // 难度: 中等
-// 同: https://leetcode-cn.com/problems/zero-matrix-lcci
-// 同: https://github.com/vjudge/leetcode/tree/master/程序员面试金典(第6版)/01.0008.零矩阵
+// 同: https://leetcode-cn.com/problems/set-matrix-zeroes/submissions
+// 同: https://github.com/vjudge/leetcode/tree/master/000001-000200/000073.矩阵置零
 var setZeroes = function(matrix) {
     let row = new Array(matrix.length).fill(false)
     let col = new Array(matrix[0].length).fill(false)
@@ -28,12 +28,12 @@ var setZeroes = function(matrix) {
     }
 };
 
-// 方法二：用时 84 ms
-// 执行用时超过了 86 %
-// 消耗内存超过了 80 %
+// 方法二：用时 88 ms
+// 执行用时超过了 77 %
+// 消耗内存超过了 78 %
 // 难度: 中等
-// 同: https://leetcode-cn.com/problems/zero-matrix-lcci
-// 同: https://github.com/vjudge/leetcode/tree/master/程序员面试金典(第6版)/01.0008.零矩阵
+// 同: https://leetcode-cn.com/problems/set-matrix-zeroes/submissions
+// 同: https://github.com/vjudge/leetcode/tree/master/000001-000200/000073.矩阵置零
 var setZeroes = function(matrix) {
     let rowFlag = false
     for (let i = 0; i < matrix[0].length; i++) {
