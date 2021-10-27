d3690eec811191c9e907b045c5336ead39700622
vjudge <gradonday@gmail.com>
2021-10-27 22:39:24 +0800

    update 5

--- 剑指Offer/0005.替换空格/5.js
+++ 剑指Offer/0005.替换空格/5.js
@@ -2,9 +2,10 @@
  * @param {string} s
  * @return {string}
  */
-// 方法一：用时 64ms
-// 执行用时超过了 89%
-// 消耗内存超过了 87%
+// 方法一：用时 64 ms
+// 执行用时超过了 89 %
+// 消耗内存超过了 87 %
+// 难度: 简单
 var replaceSpace = function(s) {
   let str = s.split(' ')
   return str.join('%20')
