9efc63c8f35ed7d3027acbfb64ab09dd80cf4218
vjudge <gradonday@gmail.com>
2021-11-15 18:59:43 +0800

    update 169go

--- 000001-000200/000169.多数元素/169.go
+++ 000001-000200/000169.多数元素/169.go
@@ -1,1 +1,32 @@
-package _00169_多数元素
+package main
+
+import "fmt"
+
+// 方法一: 用时 16 ms
+// 执行用时超过了 89 %
+// 消耗内存超过了 8 %
+// 难度: 简单
+func majorityElement(nums []int) int {
+	mergeNums := map[int]int{}
+	for _, num := range nums {
+		if _, ok := mergeNums[num]; ok {
+			mergeNums[num] ++
+		} else {
+			mergeNums[num] = 0
+		}
+	}
+
+	ret := nums[0]
+	for key, val := range mergeNums {
+		if val > mergeNums[ret] {
+			ret = key
+		}
+	}
+
+	return ret
+}
+
+func main ()  {
+	fmt.Println(majorityElement([]int{3, 2, 3}))
+	fmt.Println(majorityElement([]int{2, 2, 1, 1, 1, 2, 2}))
+}
